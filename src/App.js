badd2ed963b378df1e82fcc2211587627a7269bd
Sean Lenhart <theseanlenhart@gmail.com>
2021-06-23 11:38:05 -0400

    Adjust code structure with consts and classes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import banner from './images/header.jpeg';
 
-function App() {
-  return (
-    <div className="main_container">
-      <div className="header">
-        <img src={banner} />
-        <div classname="header_text">
-          <div className="title">Lenhart Chiropractic</div>
-          <div className="subtitle">
-            Middlefield, OH - (440) 632-1112
-          </div>
-          <div className="subtitle">
-            Hermitage, PA - (724) 347-7727
-          </div>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main_container">
+        <Header />
+        <Link />
+        <div className="main">
+          <Article />
+          <Article />
         </div>
       </div>
-      <Link />
+    );
+  }
+}
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={banner} />
+      <div classname="header_text">
+        <div className="title">Lenhart Chiropractic</div>
+        <div className="subtitle">
+          Middlefield, OH - (440) 632-1112
+        </div>
+        <div className="subtitle">
+          Hermitage, PA - (724) 347-7727
+        </div>
+      </div>
     </div>
   );
 }
 
-function Link() {
+const Link = () => {
   return (
     <div className="link_bar">
-      test
+      <div class="link">Home</div>
+      <div class="link">Middlefield, OH</div>
+      <div class="link">Hermitage, PA</div>
+      <div class="link">Videos</div>
     </div>
-  );
+  )
+};
+
+class Article extends React.Component {
+  render() {
+    return (
+      <div className="article">
+        stuff
+      </div>
+    );
+  }
 }
 
 export default App;
