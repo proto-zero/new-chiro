e6a92c43a2029ddd581df2918d2cd158062613f4
Sean Lenhart <theseanlenhart@gmail.com>
2021-06-29 15:32:39 -0400

    Adjust sidebar, format footer for mobile screens

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div className="main_container">
         <Header />
         <Link />
-        <div className="center">
+        <div className="main">
           <Article />
           <Sidebar />
         </div>
